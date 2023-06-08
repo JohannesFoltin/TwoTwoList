b3a4dbfa9117f84a6a1415661cd554ed3b8b6214
Sofie Wagner <sofie.wagner@helmholtz-berlin.de>
2023-06-08 11:44:47 +0200

    add UseCase and Service allowReadingPermission

--- src/main/java/hwr/oop/application/AllowUserToReadService.java
+++ src/main/java/hwr/oop/application/AllowUserToReadService.java
@@ -4,25 +4,25 @@ import hwr.oop.persistence.AppData;
 import hwr.oop.persistence.LoadPort;
 import hwr.oop.persistence.SavePort;
 
-public class RemoveUserAccessService implements RemoveUserAccessUseCase {
+public class AllowUserToReadService implements AllowUserToReadUseCase{
+
     private final LoadPort loadPort;
     private final SavePort savePort;
 
-    public RemoveUserAccessService(LoadPort loadPort, SavePort savePort) {
+    public AllowUserToReadService(LoadPort loadPort, SavePort savePort) {
         this.loadPort = loadPort;
         this.savePort = savePort;
     }
     @Override
-    public void removePermissionUser(Project project, User user){
+    public void addReadingPermission(Project project, User user){
         int ind = loadPort.loadData().getProjectList().indexOf(project);
         if(ind>=0) {
             AppData appData = loadPort.loadData();
-            appData.getProjectList().get(ind).removePermissionUser(user);
+            appData.getProjectList().get(ind).changePermission(user,false);
             savePort.saveData(appData);
         }
         else{
             throw new PermissionsException("project not found");
         }
     }
-
 }
---
+++ src/main/java/hwr/oop/application/AllowUserToReadUseCase.java
@@ -0,0 +1,5 @@
+package hwr.oop.application;
+
+public interface AllowUserToReadUseCase {
+    public void addReadingPermission(Project project, User user);
+}
