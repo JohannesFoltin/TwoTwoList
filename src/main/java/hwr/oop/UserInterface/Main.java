b05175770f7bdf39794b8f7813d0b462ef7aab62
Johannes <Johannes.Foltin@gmx.de>
2023-06-08 17:11:12 +0200

    delete

--- src/main/java/hwr/oop/UserInterface/Main.java
+++
@@ -1,9 +0,0 @@
-package hwr.oop.UserInterface;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Interfacemenu menu = new menu(System.in,System.out);
-        menu.start();
-    }
-}
