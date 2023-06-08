07a83e0f1f7b7917052fc0c2dff5a69b7c3f4c3d
Lukas Richter <lukas.richter.berlin@web.de>
2023-06-08 16:25:39 +0200

    UI WIP

--- src/main/java/hwr/oop/UserInterface/addProjectBuilder.java
+++ src/main/java/hwr/oop/UserInterface/addProjectBuilder.java
@@ -9,22 +9,15 @@ import java.util.Scanner;
 
 public class addProjectBuilder {
 
-<<<<<<< HEAD
 private final Scanner inputStream;
 private final PrintStream outputStream;
 private String projectName;
 private int projectId;
-=======
-    private final Scanner inputStream;
-    private final PrintStream outputStream;
-    private String projectName;
->>>>>>> 00d037f8831515a97c1b86580954a5d7883673bc
 
     public addProjectBuilder(InputStream inputStream, OutputStream outputStream) {
         this.inputStream = new Scanner(inputStream);
         this.outputStream = new PrintStream(outputStream);
     }
-<<<<<<< HEAD
     
     public String get_projectname() {
         outputStream.println("Enter a projectname:");
@@ -35,18 +28,6 @@ private int projectId;
     public int get_projectid() {
         outputStream.println("Enter a ProjectId:");
         projectId = inputStream.nextInt();
-=======
-
-    public void get_projectname() {
-        outputStream.println("Enter a projectname:");
-        projectName = inputStream.nextLine();
-        return projectName;
-    }
-
-    public String get_projectid() {
-        outputStream.println("Enter a ProjectId:");
-        String projectId = inputStream.nextLine();
->>>>>>> 00d037f8831515a97c1b86580954a5d7883673bc
         return projectId;
     }
 }
