b5c6b1f0d1ffafe18659f4fed3b820cdd357db31
Sofie Wagner <sofie.wagner@helmholtz-berlin.de>
2023-06-08 11:34:30 +0200

    added test in project for permissions

--- src/main/java/hwr/oop/application/RemoveUserAccessService.java
+++ src/main/java/hwr/oop/application/RemoveUserAccessService.java
@@ -1,17 +1,17 @@
 package hwr.oop.application;
 
-import hwr.oop.persistence.AppData;
 import hwr.oop.persistence.LoadPort;
 import hwr.oop.persistence.SavePort;
 
-public class PermissionsService implements PermissionsUseCase{
+public class RemoveUserAccessService implements RemoveUserAccessUseCase {
     private final LoadPort loadPort;
     private final SavePort savePort;
 
-    public PermissionsService(LoadPort loadPort, SavePort savePort) {
+    public RemoveUserAccessService(LoadPort loadPort, SavePort savePort) {
         this.loadPort = loadPort;
         this.savePort = savePort;
     }
+    @Override
     public void removePermissionUser(Project project, User user){
         int ind = loadPort.loadData().getProjectList().indexOf(project);
         if(ind>=0) {
---
+++ src/main/java/hwr/oop/application/RemoveUserAccessUseCase.java
@@ -0,0 +1,5 @@
+package hwr.oop.application;
+
+public interface RemoveUserAccessUseCase {
+    public void removePermissionUser(Project project, User user);
+}
