d398de5515412e4f459618bd9141ddbe757d2c36
Johannes <Johannes.Foltin@gmx.de>
2023-06-10 14:22:31 +0200

    implemented Mock

--- src/test/java/hwr/oop/userinterfaceTests/LoginTest.java
+++ src/test/java/hwr/oop/userinterfaceTests/LoginTest.java
@@ -9,6 +9,10 @@ import hwr.oop.userinterface.Login;
 import hwr.oop.userinterface.MainMenu;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -18,15 +22,18 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class LoginTest {
     AppData appDataMock;
-    private MyMainMenu mainMenu;
+    @Mock
+    private MainMenu mainMenu;
     private ValidateUserUseCase validateUserUseCase;
     @BeforeEach
     void setUp(){
         appDataMock = new AppData(new ArrayList<>(),new ArrayList<>());
-        mainMenu = new MyMainMenu();
+        mainMenu = mock();
         LoadPort loadPort = () -> appDataMock;
 
         validateUserUseCase = new ValidateUserService(loadPort);
@@ -42,12 +49,13 @@ class LoginTest {
 
         login.start();
 
-        String output = retrieveResultFrom(outputStream); //This is how you can get the result output (sadly the complete, not the last line!)
-        assertThat(mainMenu.isCalled()).isTrue();
-        assertThat(output).isEqualTo("o you wanna do??????\n" +
+        String output = outputStream.toString(); //This is how you can get the result output (sadly the complete, not the last line!)
+        verify(mainMenu).start(any());
+        assertThat(output).isEqualTo("What do you wanna do??????\n" +
                 "Type 1 to login\n" +
                 "Type 2 to register a new user\n" +
-                "Enter Username:");
+                "Enter Username:\n" +
+                "\n");
 
     }
     @Test
@@ -62,8 +70,8 @@ class LoginTest {
 
         login.start();
 
-        String output = retrieveResultFrom(outputStream); //This is how you can get the result output (sadly the complete, not the last line!)
-        assertThat(mainMenu.isCalled()).isTrue();
+        String output = outputStream.toString(); //This is how you can get the result output (sadly the complete, not the last line!)
+        verify(mainMenu).start(any());
         assertThat(output).isEqualTo("o you wanna do??????\n" +
                 "Type 1 to login\n" +
                 "Type 2 to register a new user\n" +
@@ -77,26 +85,9 @@ class LoginTest {
 
     }
 
-    private String retrieveResultFrom(OutputStream outputStream) {
-        String outputText = outputStream.toString();
-        String key = "output:";
-        return outputText.substring(outputText.indexOf(key) + key.length()).trim();
-    }
-
     private InputStream createInputStreamForInput(String input) {
         byte[] inputInBytes = input.getBytes();
         return new ByteArrayInputStream(inputInBytes);
     }
 
-    private static class MyMainMenu extends MainMenu {
-        public boolean isCalled() {
-            return isCalled;
-        }
-
-        private boolean isCalled = false;
-        @Override
-        public void start(User user){
-            isCalled = true;
-        }
-    }
 }
