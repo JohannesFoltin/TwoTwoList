59bb8fdb773a10879a7da84c50466215e6ed1098
Julian-He <heruthjulian@gmail.com>
2023-06-10 23:29:28 +0200

    added parameters and constructor

--- src/main/java/hwr/oop/userinterface/CreateProjectMenu.java
+++ src/main/java/hwr/oop/userinterface/CreateProjectMenu.java
@@ -1,6 +1,26 @@
 package hwr.oop.userinterface;
 
+import hwr.oop.application.CreateProjectUseCase;
+import hwr.oop.application.Task;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Optional;
+import java.util.Scanner;
+
 public class CreateProjectMenu {
+
+    private final Scanner input;
+    private final PrintStream output;
+    private final CreateProjectUseCase createProjectUseCase;
+
+    public CreateProjectMenu(InputStream input, OutputStream output, CreateProjectUseCase createProjectUseCase) {
+        this.input = new Scanner(input);
+        this.output = new PrintStream(output);
+        this.createProjectUseCase = createProjectUseCase;
+    }
+
     public void start() {
     }
 }
