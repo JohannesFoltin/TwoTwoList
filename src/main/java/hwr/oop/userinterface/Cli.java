8a016ed00aa2e183c837e218896ce82bd664cbcc
Johannes <Johannes.Foltin@gmx.de>
2023-06-11 19:44:13 +0200

    CLI

--- src/main/java/hwr/oop/userinterface/Cli.java
+++ src/main/java/hwr/oop/userinterface/Cli.java
@@ -1,5 +1,18 @@
 package hwr.oop.userinterface;
 
+import hwr.oop.application.CreateUserService;
+import hwr.oop.application.ValidateUserService;
+import hwr.oop.persistence.LoadPort;
+import hwr.oop.persistence.SavePort;
+
+
 public class Cli {
+    private final Login login;
+    public Cli(LoadPort loadPort, SavePort savePort) {
+        login = new Login(System.in,System.out,new MainMenu(),new ValidateUserService(loadPort),new CreateUserService(savePort,loadPort));
+    }
+    public void startCli(){
+        login.start();
+    }
 
 }
