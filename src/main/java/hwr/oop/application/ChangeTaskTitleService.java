483b7b35e62bfa953d12d4ca29dad99439a9227e
Johannes <Johannes.Foltin@gmx.de>
2023-06-11 18:00:33 +0200

    johannes entered the game vol 2

--- src/main/java/hwr/oop/application/ChangeTaskTitleService.java
+++ src/main/java/hwr/oop/application/ChangeTaskTitleService.java
@@ -5,25 +5,6 @@ import hwr.oop.persistence.LoadPort;
 import hwr.oop.persistence.SavePort;
 
 public class ChangeTaskTitleService implements ChangeTaskTitleUseCase {
-    private final LoadPort loadPort;
-    private final SavePort savePort;
 
-    public ChangeTaskTitleService(LoadPort loadPort, SavePort savePort) {
-        this.loadPort = loadPort;
-        this.savePort = savePort;
-    }
-
-    @Override
-    public void changeTitle(Task task, String newTitle){
-        int ind = loadPort.loadData().().indexOf(task);
-        if(ind>=0){
-            AppData appData= loadPort.loadData();
-            appData.getTaskList().get(ind).changeTitle(newTitle);
-            savePort.saveData(appData);
-        }
-        else{
-            throw new ChangeTitleException("Task not found");
-        }
-    }
     
 }
--- src/main/java/hwr/oop/application/ChangeTaskTitleUseCase.java
+++ src/main/java/hwr/oop/application/ChangeTaskTitleUseCase.java
@@ -4,10 +4,9 @@ public interface ChangeTaskTitleUseCase {
     void changeTitleInProject(Task task, String newTitle,Project project);
     void changeContentInProject(Task task, String newContent,Project project);
     void changeTaskStateInProject(Task task, TaskState taskState ,Project project);
-    void changeDeadlineInProject(Task task, String newTitle,Project project);
-    void changeTitleInUser(Task task, String newTitle);
-    void changeContentInUser(Task task, String newTitle);
-    void changeTaskStateInUser();
-    void changeDeadlineInUser();
-
+    void changeDeadlineInProject(Task task, String newDeadline,Project project);
+    void changeTitleInUser(Task task, String newTitle,User user);
+    void changeContentInUser(Task task, String newContent,User user);
+    void changeTaskStateInUser(Task task, TaskState taskState ,User user);
+    void changeDeadlineInUser(Task task, String newDeadline ,User user);
 }
