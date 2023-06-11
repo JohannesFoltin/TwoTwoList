fb36d867a8e2d8afb17ff6a50bbb3531c369bf88
Julian-He <heruthjulian@gmail.com>
2023-06-11 02:39:07 +0200

    added attributes and constructor

--- src/main/java/hwr/oop/userinterface/EditProjectMenu.java
+++ src/main/java/hwr/oop/userinterface/EditProjectMenu.java
@@ -1,9 +1,35 @@
 package hwr.oop.userinterface;
 
 import hwr.oop.application.Project;
+import hwr.oop.application.Task;
 import hwr.oop.application.User;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class EditProjectMenu {
-    public void start(User user, Project project) {
+    private final Scanner input;
+    private final PrintStream output;
+    private final EditTaskMenu editTaskMenu;
+    private final AddTaskToProjectMenu addTaskToProjectMenu;
+    private final EditProjectPermissionsMenu editProjectPermissionsMenu;
+
+    public EditProjectMenu(InputStream input, OutputStream output,
+                           EditTaskMenu editTaskMenu,
+                           AddTaskToProjectMenu addTaskToProjectMenu,
+                           EditProjectPermissionsMenu editProjectPermissionsMenu) {
+        this.input = new Scanner(input);
+        this.output = new PrintStream(output);
+        this.editTaskMenu = editTaskMenu;
+        this.addTaskToProjectMenu = addTaskToProjectMenu;
+        this.editProjectPermissionsMenu = editProjectPermissionsMenu;
     }
+
+    public void start(User user, Project project) {
+
+    }
+
+
 }
---
+++ src/main/java/hwr/oop/userinterface/EditProjectPermissionsMenu.java
@@ -0,0 +1,4 @@
+package hwr.oop.userinterface;
+
+public class EditProjectPermissionsMenu {
+}
